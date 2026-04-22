eeef04ec2b8267f99c116667e815ce79682d4778
elielalvespinheiro <elielalvespinheiro3456@gmail.com>
2026-04-21 20:39:02 -0400

    Implementação das funcionalidades de validação de imagens

--- backend/routes/analyze.py
+++ backend/routes/analyze.py
@@ -1,9 +1,14 @@
 from fastapi import APIRouter, UploadFile, File, HTTPException
 from models.detector import detect_disease
 from database.database import save_analysis
+from PIL import Image
+from io import BytesIO
 import uuid, time
 
 router = APIRouter()
+
+MAX_SIZE = 10 * 1024 * 1024
+ALLOWED_FORMATS = ["JPEG", "PNG", "WEBP"]
 
 @router.post("/analyze")
 async def analyze_image(file: UploadFile = File(...)):
@@ -16,13 +21,29 @@ async def analyze_image(file: UploadFile = File(...)):
     - confidence: porcentagem de confiança (0.0 a 1.0)
     - analysis_id: ID para consulta no histórico
     """
-    if file.content_type not in ["image/jpeg", "image/png", "image/webp"]:
-        raise HTTPException(status_code=400, detail="Formato de imagem inválido. Use JPG, PNG ou WEBP.")
 
+    if not file:
+        raise HTTPException(status_code=400, detail="Nenhum arquivo foi enviado.")
+    
     image_bytes = await file.read()
+
+    if len(image_bytes) > MAX_SIZE:
+        raise HTTPException(status_code=400, detail="A imagem passou do tamanho maximo de 10MB")
+
+    try:
+        image = Image.open(BytesIO(image_bytes))
+        image.verify()
+        image = Image.open(BytesIO(image_bytes))
+
+    except  Exception:
+        raise HTTPException(status_code=400, detail="O arquivo enviado não é uma imagem válida!")
+    
+    if image.format not in ALLOWED_FORMATS:
+        raise HTTPException(status_code=400, detail="O formato do arquivo não é permitido. Use somente JPEG, PNG e WEBP")
 
     try:
         result = detect_disease(image_bytes)
+        
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Erro no modelo de IA: {str(e)}")
 
